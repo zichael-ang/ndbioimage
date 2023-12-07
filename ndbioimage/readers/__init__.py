735523aba069ced7ec3c2db40eafecbdbacd94d4
Wim Pomp <w.pomp@nki.nl>
2023-12-07 13:52:06 +0100

    - Static __all__ for pyinstaller compatibility.

--- ndbioimage/readers/__init__.py
+++ ndbioimage/readers/__init__.py
@@ -1,4 +1,1 @@
-from pathlib import Path
-
-__all__ = [file.stem for file in Path(__file__).parent.iterdir()
-           if file.suffix == ".py" and not file == Path(__file__) and not file.stem.startswith('.')]
+__all__ = 'bfread', 'cziread', 'fijiread', 'ndread', 'seqread', 'tifread'
